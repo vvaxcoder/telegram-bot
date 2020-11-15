fab7c5142e3b151ebde7310f5a7b7e7dbd5f618c
Valerii Voronkov <scorpirat@gmail.com>
2020-11-15 11:43:25 +0300

    Add nodemon;

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const Telegraf = require("telegraf");
 const { token } = require("./config");
 const bot = new Telegraf(token);
 
+//run the bot
+bot.start(ctx => ctx.reply("Start command"));
+bot.help(ctx => ctx.reply("Help command"));
+bot.settings(ctx => ctx.reply("Settings command"));
+
 bot.launch().then(result => console.log("Bot has started")).catch(err => console.log(err));
