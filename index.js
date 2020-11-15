e33a9b2c2b07f44455b9576241b2c58ec2b30745
Valerii Voronkov <scorpirat@gmail.com>
2020-11-15 12:02:46 +0300

    Add command handler.

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ bot.start(ctx => ctx.reply("Start command"));
 bot.help(ctx => ctx.reply("Help command"));
 bot.settings(ctx => ctx.reply("Settings command"));
 
+// bot.command("stop", (ctx) => {
+//     return ctx.reply("Stop command");
+// });
+bot.command(["stop", "finish"], (ctx) => {
+    return ctx.reply("Stop command");
+});
+
 bot.launch().then(result => console.log("Bot has started")).catch(err => console.log(err));
