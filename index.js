4729b2cd17fa932488b29c7e92229ad1232f014e
Valerii Voronkov <scorpirat@gmail.com>
2020-11-15 12:16:00 +0300

    Add mention, phone and hashtag handlers.

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ bot.start(ctx => ctx.reply("Start command"));
 bot.help(ctx => ctx.reply("Help command"));
 bot.settings(ctx => ctx.reply("Settings command"));
 
+//use string
 // bot.command("stop", (ctx) => {
 //     return ctx.reply("Stop command");
 // });
+
+//use array of string
 bot.command(["stop", "finish"], (ctx) => {
     return ctx.reply("Stop command");
+});
+
+bot.mention("botfather", (ctx) => {
+    return ctx.reply("Botfather mentioned");
+});
+
+bot.phone("+797878787878", (ctx) => {
+    return ctx.reply("Phone number");
+});
+
+bot.hashtag("bot", (ctx) => {
+    return ctx.reply("bot hashtaged");
 });
 
 bot.launch().then(result => console.log("Bot has started")).catch(err => console.log(err));
