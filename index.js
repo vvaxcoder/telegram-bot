2ed2ee37e7d030f9ff675a228c2c3df0e70a1ab5
Valerii Voronkov <scorpirat@gmail.com>
2020-12-13 13:04:53 +0300

    Add middleware.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const Telegraf = require("telegraf");
 const { token } = require("./config");
 const bot = new Telegraf(token);
 
+//use middlewarw BEFORE your code
+bot.use(async (ctx, next) => {
+  ctx.reply("Middleware");
+  await next(ctx);
+});
+
 //run the bot
 bot.start(ctx => ctx.reply("Start command"));
 bot.help(ctx => ctx.reply("Help command"));
@@ -13,25 +19,28 @@ bot.settings(ctx => ctx.reply("Settings command"));
 // });
 
 //use array of string
-bot.command(["stop", "finish"], (ctx) => {
-    return ctx.reply("Stop command");
+bot.command(["stop", "finish"], ctx => {
+  return ctx.reply("Stop command");
 });
 
-bot.mention("botfather", (ctx) => {
-    return ctx.reply("Botfather mentioned");
+bot.mention("botfather", ctx => {
+  return ctx.reply("Botfather mentioned");
 });
 
-bot.phone("+797878787878", (ctx) => {
-    return ctx.reply("Phone number");
+bot.phone("+797878787878", ctx => {
+  return ctx.reply("Phone number");
 });
 
-bot.hashtag("bot", (ctx) => {
-    return ctx.reply("bot hashtaged");
+bot.hashtag("bot", ctx => {
+  return ctx.reply("bot hashtaged");
 });
 
-bot.command("ctx", (ctx) => {
-    console.log(ctx);
-    return ctx.reply("ok");
+bot.command("ctx", ctx => {
+  console.log(ctx);
+  return ctx.reply("ok");
 });
 
-bot.launch().then(result => console.log("Bot has started")).catch(err => console.log(err));
+bot
+  .launch()
+  .then(result => console.log("Bot has started"))
+  .catch(err => console.log(err));
