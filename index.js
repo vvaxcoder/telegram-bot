c7aea9cdc6c7f9da218a5d4ccf7b2f8f3b565f27
Valerii Voronkov <scorpirat@gmail.com>
2020-12-13 17:58:42 +0300

    Install currency-codes library.

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
 const Telegraf = require("telegraf");
 const { token } = require("./config");
 const bot = new Telegraf(token);
-
-//use middleware BEFORE your code
-bot.use(async (ctx, next) => {
-  ctx.reply("Middleware");
-  ctx.state.isSend = true;
-  await next(ctx);
-});
+const axios = require("axios");
 
 //run the bot
 bot.start(ctx => {
-    const { state } = ctx;
-    return ctx.reply(`Start command ${state.isSend}`);
-});
-bot.help(ctx => ctx.reply("Help command"));
-bot.settings(ctx => ctx.reply("Settings command"));
-
-//use string
-// bot.command("stop", (ctx) => {
-//     return ctx.reply("Stop command");
-// });
-
-//use array of string
-bot.command(["stop", "finish"], ctx => {
-  return ctx.reply("Stop command");
+    return ctx.reply('Welcome Monobank telegramm bot');
 });
 
-bot.mention("botfather", ctx => {
-  return ctx.reply("Botfather mentioned");
+bot.hears("currency", async (ctx) => {
+  try {
+    const response = await axios.get("https://api.monobank.ua/bank/currency");
+    // console.log(response);
+    const currencyArr = response.data;
+    return ctx.reply(response.data[0]);
+  } catch (error) {
+    return ctx.reply("Error on request");
+  }
 });
 
-bot.phone("+797878787878", ctx => {
-  return ctx.reply("Phone number");
-});
-
-bot.hashtag("bot", ctx => {
-  return ctx.reply("bot hashtaged");
-});
-
-bot.command("ctx", ctx => {
-  console.log(ctx);
-  return ctx.reply("ok");
-});
-
-bot.hears('dog', (ctx) => {
-    ctx.reply('whatever');
-});
-
-bot
-  .launch()
-  .then(result => console.log("Bot has started"))
-  .catch(err => console.log(err));
+bot.startPolling();
